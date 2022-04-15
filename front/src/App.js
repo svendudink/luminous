9ae34ef2a7703921241c1ec14404aa69e741babc
Sven Dudink <sven@dudink.net>
2022-04-15 15:01:11 +0200

    new commit nr2

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import { serial } from "./helper/serial";
+import {
+  Router,
+  Route,
+  BrowserRouter,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
   return <div className="App"></div>;
