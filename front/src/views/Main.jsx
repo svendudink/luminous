5524ee5b812bdd6137695031563b7a304b78b5f0
Sven Dudink <sven@dudink.net>
2022-04-30 23:07:21 +0200

    Import  image preloader

--- front/src/views/Main.jsx
+++ front/src/views/Main.jsx
@@ -1,3 +1,8 @@
+import { useImagePreloader } from "../hooks/preLoadImage";
+import { preloadSrcList } from "../media/backgrounds/imageLoad/imageArray";
+
 export default function Main() {
+  const preLoad = useImagePreloader(preloadSrcList).percentage;
+
   return <div>Main</div>;
 }
