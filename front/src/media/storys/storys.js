593a1895bf6c32b9d6f3f4b18b48af2ced513cac
Sven Dudink <sven@dudink.net>
2022-05-14 11:22:36 +0200

    addd fiirst story

--- front/src/media/storys/storys.js
+++ front/src/media/storys/storys.js
@@ -1,2 +1,7 @@
-const [textColor, setTextColor] = useState("#195B8E");
-const storyArray = { color:};
+
+const Storys = (props) => { 
+
+    const [textColor, setTextColor] = useState("#195B8E");
+const storyArray = { color: textColor, story: };
+
+}
