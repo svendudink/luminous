fb31fe270e8d3ab9eb5295c64198a546d3825eb6
Sven Dudink <sven@dudink.net>
2022-05-17 13:17:37 +0200

    add 3rd story

--- front/src/media/storys/storys.js
+++ front/src/media/storys/storys.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
-
-export const Storys = (props) => { 
-
-    const [textColor, setTextColor] = useState("#195B8E");
-const storyArray = [
-  {
-        color: textColor,
-      storyName:
-    story: `Bottle Luminous has a total of 132 Bottles, all connected over a
+export const Storys = (props) => {
+  const [textColor, setTextColor] = useState("#195B8E");
+  const storyArray = [
+    {
+      color: textColor,
+      storyName: "mesh",
+      story: `Bottle Luminous has a total of 132 Bottles, all connected over a
         Blueooth MESH network.`,
-  },
-  {
-    color: textColor,
-    story: `Bottle Luminous has a total of 132 Bottles, all connected over a
+    },
+    {
+      color: textColor,
+      storyName: "lmap1",
+      story: `Bottle Luminous has a total of 132 Bottles, all connected over a
         Blueooth MESH network.`,
-  },
-];
-
-}
-
+    },
+    {
+      color: textColor,
+      storyName: "lamp2",
+      story: `Completely waterproof as bulbs are sealed inside glass`,
+    },
+  ];
+};
