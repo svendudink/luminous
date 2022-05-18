4be1462bb61f07e3a8d60dc6d692a4d648398900
Sven Dudink <sven@dudink.net>
2022-05-18 18:06:12 +0200

    add story 5

--- front/src/media/storys/storys.js
+++ front/src/media/storys/storys.js
@@ -33,5 +33,18 @@ export const Storys = (props) => {
         <Button onClick={() => setLampBackground("lamp3c")}>Blue</Button>
       </div>`,
     },
+    {
+      color: textColor,
+      storyName: "lamp4",
+      story: `<div
+        style={{ color: textColor }}
+        onMouseLeave={() => setLampBackground("startPagewhitea")}
+      >
+        <p1> 16.7 Million colours to choose from, try a few </p1>
+        <Button onClick={() => setLampBackground("lamp3b")}>Red</Button>
+        <Button onClick={() => setLampBackground("lamp3d")}>Yellow</Button>
+        <Button onClick={() => setLampBackground("lamp3c")}>Blue</Button>
+      </div>`,
+    },
   ];
 };
