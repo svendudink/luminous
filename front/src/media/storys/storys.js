addb550c6ebf444cdcb6e9b4733d931754c35401
Sven Dudink <sven@dudink.net>
2022-05-20 07:28:17 +0200

    Story 6

--- front/src/media/storys/storys.js
+++ front/src/media/storys/storys.js
@@ -4,25 +4,25 @@ export const Storys = (props) => {
   const [textColor, setTextColor] = useState("#195B8E");
   const storyArray = [
     {
-      color: textColor,
       storyName: "mesh",
+      color: textColor,
       story: `Bottle Luminous has a total of 132 Bottles, all connected over a
         Blueooth MESH network.`,
     },
     {
-      color: textColor,
       storyName: "lmap1",
+      color: textColor,
       story: `Bottle Luminous has a total of 132 Bottles, all connected over a
         Blueooth MESH network.`,
     },
     {
-      color: textColor,
       storyName: "lamp2",
+      color: textColor,
       story: `Completely waterproof as bulbs are sealed inside glass`,
     },
     {
-      color: textColor,
       storyName: "lamp3",
+      color: textColor,
       story: `<div
         style={{ color: textColor }}
         onMouseLeave={() => setLampBackground("startPagewhitea")}
@@ -34,16 +34,31 @@ export const Storys = (props) => {
       </div>`,
     },
     {
-      color: textColor,
       storyName: "lamp4",
+      color: textColor,
       story: `Bulbs are all controlled from 1 Rooted android device, which is in turn
         controlled by the NodeJS server`,
     },
     {
-      color: textColor,
       storyName: "mapManager",
-      story: `Bulbs are all controlled from 1 Rooted android device, which is in turn
-        controlled by the NodeJS server`,
+      color: "black",
+      story: `Select a map to change the colors or<br></br>
+        Create your own event map <br></br>
+        this will be the first step setting up a light event`,
+    },
+    {
+      storyName: "mapManager",
+      color: "black",
+      story: `Select a map to change the colors or<br></br>
+        Create your own event map <br></br>
+        this will be the first step setting up a light event`,
+    },
+    {
+      storyName: "mapManager",
+      color: "bulbId",
+      story: `Select a map to change the colors or<br></br>
+        Create your own event map <br></br>
+        this will be the first step setting up a light event`,
     },
   ];
 };
