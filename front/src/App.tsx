b444a7184d90599b47fac30d46734088e6c90db5
Sven Dudink <sven@dudink.net>
2022-05-28 05:24:15 +0200

    add aloowsynteticimports

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import About from "./views/About";
 // import Home from "./views/Home";
