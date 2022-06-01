d95230b12c04176c95b148e4a0d0bb1a2669f0e1
Sven Dudink <sven@dudink.net>
2022-06-01 09:28:18 +0200

    Add import react to controller

--- front/src/views/About.tsx
+++ front/src/views/About.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useContext, useEffect } from "react";
 // import { GlobalContext } from "../components/context/GlobalContext";
 import { AboutStory } from "../media/Storys/Storys";
