d3c5b03c28de37c56410ccbd00a9d7e281d103d5
Sven Dudink <sven@dudink.net>
2022-06-06 13:11:56 +0200

    added app bar

--- front/src/views/Main.jsx
+++ front/src/views/Main.jsx
@@ -4,7 +4,7 @@ import { useImagePreloader } from "../hooks/preLoadImage";
 import { preloadSrcList } from "../media/imageLoad/imageArray";
 
 export default function Main() {
-  const preLoad = useImagePreloader(preloadSrcList).percentage;
+  const preLoad = useImagePreloader(preloadSrcList).imagesPreloaded;
 
-  return <div>Main</div>;
+  return preLoad && <div>Main</div>;
 }
