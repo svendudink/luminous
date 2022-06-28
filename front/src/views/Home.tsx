6199096976c4d7af30a55542dedde6fef5ab80e3
Sven Dudink <sven@dudink.net>
2022-06-28 09:56:57 +0200

    chains main

--- front/src/views/Home.tsx
+++ front/src/views/Home.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+
+import { useImagePreloader } from "../hooks/preLoadImage";
+import { preloadSrcList } from "../media/imageLoad/imageArray";
+
 export default function Home() {
-  return <div>home field</div>;
+  const preLoad = useImagePreloader(preloadSrcList).imagesPreloaded;
+
+  return preLoad && <div>Main</div>;
 }
--- front/src/views/Main.jsx
+++ front/src/views/Main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
 
-import { useImagePreloader } from "../hooks/preLoadImage";
-import { preloadSrcList } from "../media/imageLoad/imageArray";
-
-export default function Main() {
-  const preLoad = useImagePreloader(preloadSrcList).imagesPreloaded;
-
-  return preLoad && <div>Main</div>;
-}
