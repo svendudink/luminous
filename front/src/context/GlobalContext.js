6934c6fde323a23ff7aa9113f97bc6606a6407f2
Sven Dudink <sven@dudink.net>
2022-07-07 03:04:36 +0200

    imported global context provider

--- front/src/context/GlobalContext.js
+++ front/src/context/GlobalContext.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const globalContext = createContext();
 
-export const globalContextProvider = (props) => {
+export const GlobalContextProvider = (props) => {
   return (
     <globalContext.Provider value={{}}>{props.children}</globalContext.Provider>
   );
